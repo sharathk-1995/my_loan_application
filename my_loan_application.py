1421e1bd2d7ffd4073e3c76c5d9a4073d3db314f
hp <sharath_k1@outlook.com>
2020-03-01 15:46:55 +0530

    my_loan_application app deployment

---
+++ my_loan_application.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def main():
+	st.title("Building a simple ML application")
+	st.subheader("ML application")
+st.text("python programming language")
+
+if __name__ == '__main__':
+	main()
